a748b1ca10c7e6ce76833bc5e8f5e020ed61399a
Nina Wetzig <nina.wetzig@web.de>
2018-08-15 16:21:09 +0200

    creating functions to read in the profile csv and the J1 and J2 csv and a function to combine J1 and J2 into one csv table

--- source/read_data.R
+++ source/read_data.R
@@ -1,31 +1,67 @@
-names <- c("Marker", "HeLa", "Glenn", "Sarah" , "Trine" , "Nicole")
+#' params: profile_file - a profile csv file
+#' return: data.frame of the prfile file
+read_profile <- function(profile_file){
+  profiles.names <- c("Marker", "HeLa", "Glenn", "Sarah" , "Trine" , "Nicole")
+  profiles <- read.csv(profile_file, na.strings=c("","NA"), sep =",", col.names = profiles.names, header = TRUE, stringsAsFactors = FALSE)
+  # munging
+  profiles$Marker <- gsub(pattern = "\n", replacement="", x = profiles$Marker)
+  profiles$Marker <- gsub(pattern = " ", replacement="", x = profiles$Marker)
+  return(profiles)
+}
 
-profiles <- read.csv("resources/profiles.csv", na.strings=c("","NA"), sep =",", col.names = names, header = TRUE, stringsAsFactors = FALSE)
+read_Jinput <- function(j_input_file){
+  names2 <- c("Marker", "Allele" , "Pattern" , "Pattern_Quality" , "Reads", "Variants")
+  j_input <- read.csv(j_input_file, na.strings=c("","NA"), sep = ",", col.names = names2, header = FALSE, stringsAsFactors = FALSE,colClasses = c("character", "character", "character", "numeric", "numeric", "character"))
+  invisible(j_input)
+}
 
-names2 <- c("Marker", "Allele" , "Pattern" , "Pattern_Quality" , "Reads", "Variants")
+read_data <- function(profile_file, j1in, j2in){
+  profiles = read_profile(profile_file)
+  J1_input = read_Jinput(j1in)
+  J2_input = read_Jinput(j2in)
+  return(profiles)
+}
 
-J1_input <- read.csv("data/Run_01/variant_calling/J1_STR (copy).csv", na.strings=c("","NA"), sep = ",", col.names = names2, header = TRUE, stringsAsFactors = FALSE)
-J2_input <- read.csv("data/Run_01/variant_calling/J2_STR.out", na.strings=c("","NA"), sep = ",", col.names = names2, header = TRUE, stringsAsFactors = FALSE)
-
-# munging
-profiles$Marker <- gsub(pattern = "\n", replacement="", x = profiles$Marker)
-profiles$Marker <- gsub(pattern = " ", replacement="", x = profiles$Marker)
-
-
-vWA <- J1_input[J1_input$Marker == "vWA", ]
-TH01 <- J1_input[J1_input$Marker == "TH01", ]
-c <- rbind(vWA, TH01)
-head(subset(J1_input, Marker %in% c("vWA", "TH01")))
-# mit subset kann die Tabelle leichter erstellt werden
-
-
-#J1_input$Sample <- "J1"
-#J2_input$Sample <- "J2"
-J1_input <- cbind(Sample = "J1" ,J1_input )
-J2_input <- cbind(Sample = "J2" ,J2_input )
-#Beides fügt der Tabelle eine neue Spalte hinzu. Die ersten Befehle an letzer Stelle, die letzen Befehle als erste Spalte
-
-J_input <- rbind(J1_input, J2_input) 
-
-
-write.csv(J_input, file = "results/J_STR.csv", na = "", sep =",", quote = FALSE, row.names = FALSE)
+ignoreme <- function(){
+  read_data("resources/profiles.csv", "data/Run_01/variant_calling/J1_STR (copy).csv", "data/Run_01/variant_calling/J2_STR.out")
+  
+  
+  profiles.names <- c("Marker", "HeLa", "Glenn", "Sarah" , "Trine" , "Nicole")
+  
+  profiles <- read.csv("resources/profiles.csv", na.strings=c("","NA"), sep =",", col.names = profiles.names, header = TRUE, stringsAsFactors = FALSE)
+  
+  names2 <- c("Marker", "Allele" , "Pattern" , "Pattern_Quality" , "Reads", "Variants")
+  
+  J1_input <- read.csv("data/Run_01/variant_calling/J1_STR (copy).csv", na.strings=c("","NA"), sep = ",", col.names = names2, header = FALSE, stringsAsFactors = FALSE,colClasses = c("character", "character", "character", "numeric", "numeric", "character"))
+  J2_input <- read.csv("data/Run_01/variant_calling/J2_STR.out", 
+                       na.strings=c("","NA"), 
+                       sep = ",", 
+                       col.names = names2, 
+                       header = FALSE, 
+                       stringsAsFactors = FALSE,
+                       colClasses = c("character", "character", "character", "character", "numeric", "character"))
+  
+  
+  
+  vWA <- J1_input[J1_input$Marker == "vWA", ]
+  TH01 <- J1_input[J1_input$Marker == "TH01", ]
+  c <- rbind(vWA, TH01)
+  head(subset(J1_input, Marker %in% c("vWA", "TH01")))
+  # mit subset kann die Tabelle leichter erstellt werden
+}
+  
+create_csv <- function(j1in,j2in){
+  J1_input = read_Jinput(j1in)
+  J2_input = read_Jinput(j2in)
+  J1_input$Sample <- "J1"
+  J2_input$Sample <- "J2"
+  #J1_input <- cbind(Sample = "J1" ,J1_input )
+  #J2_input <- cbind(Sample = "J2" ,J2_input )
+  #Beides fügt der Tabelle eine neue Spalte hinzu. Die ersten Befehle an letzer Stelle, die letzen Befehle als erste Spalte
+  
+  J_input <- rbind(J1_input, J2_input) 
+  
+  
+  J_csv <- write.csv(J_input, file = "results/J_STR_3.csv", na = "", quote = FALSE, row.names = FALSE)
+  return("results/J_STR_3.csv")
+}
