564b8fc056ad46faeb36c061edb7ad8403a79cfe
Nina Wetzig <nina.wetzig@web.de>
2018-08-23 09:36:11 +0200

    added the new function(to compare columns) from read_data

--- source/strstats.R
+++ source/strstats.R
@@ -15,8 +15,13 @@ option_list = list(
   make_option(c("-z", "--jinput2"), type="character", default=NULL, 
               help="output file name [default= %default]", metavar="character"),
   make_option(c("-r", "--rawinput"), type="character", default=NULL, 
-              help="aggregates data frames and sums up the column 'Reads' per 'Marker' and per 'Marker' and 'Allele'.output = data frame", metavar="character")
-  
+              help="aggregates data frames and sums up the column 'Reads' per 'Marker' and per 'Marker' and 'Allele'.output = data frame", metavar="character"),
+  make_option(c("-u", "--Run01"), type="character", default=NULL, 
+              help="tabel with samples, marker", metavar="character"),
+  make_option(c("-o", "--profilefile"), type="character", default=NULL, 
+              help="profiles tabel with patients", metavar="character"),
+  make_option(c("-g", "--global_sample"), type="character", default=NULL, 
+              help="table with patients and samples", metavar="character")
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
@@ -32,7 +37,7 @@ if (!is.null(opt$profile)){
 } else {}
 
 # Input-Tabelle zu beliebigem Sample
-if (!is.null(opt$jinput)){ 
+if (!is.null(opt$jinput)){
   print("J_input")
   read_Jinput(opt$jinput)
 } else {}
@@ -48,3 +53,14 @@ if (!is.null(opt$rawinput)){
   print("Tabellen:")
   print(read_raw_input(opt$rawinput))
 } else{}
+
+#source/strstats.R -p resources/profiles.csv  -j data/Run_01/variant_calling/J1_STR.out -e data/Run_01/variant_calling/J1_STR.out -z data/Run_01/variant_calling/J2_STR.out -r /home/nina/Downloads/Run_01_short.out
+
+
+#Für die Funktion zur Zuordnung der Marker, Samples und Patienten und zur Abgleichung der callAllele mit den korrekten Allelen
+if (!is.null(opt$Run01) & !is.null(opt$profilefile) & !is.null(opt$global_sample)){ 
+  print("callAllele/trueAllele")
+  compare_call_to_Allele(opt$Run01, opt$profilefile, opt$global_sample)
+} else {}
+
+#source/strstats.R -u /home/nina/Downloads/Run_01_short.out -o resources/profiles.csv -g resources/global_sample_overview_forensic.csv
